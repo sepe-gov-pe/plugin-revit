2daa9035e9c2e63353cbf105eb4cebe199c6a124
Rodrigo Teixeira <rdrgotxra@gmail.com>
2026-05-22 12:17:56 -0300

    correção

--- startup.py
+++ startup.py
@@ -2,7 +2,7 @@ import subprocess
 
 try:
     result = subprocess.check_output(
-        "pyrevit extensions update SEPE-Revit --debug",
+        "pyrevit extensions update plugin-revit --debug",
         shell=True,
         creationflags=0x08000000,
     )
