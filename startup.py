975edd40fbd0201b9e75819bc1dfe4ccfd5b35f8
Rodrigo Teixeira <rdrgotxra@gmail.com>
2026-06-01 23:21:56 -0300

    teste

--- startup.py
+++ startup.py
@@ -1,17 +1,24 @@
 import os
 import subprocess
 
+from pyrevit import extensions
+
 pasta = os.path.dirname(os.path.abspath(__file__))
-flag = os.path.join(pasta, "DEV")
-atualizar = not os.path.exists(flag)
+dev_flag = os.path.join(pasta, "DEV")
+atualizar = not os.path.exists(dev_flag)
 
 if atualizar:
+    print("atualizou")
     try:
         subprocess.check_output(
             "pyrevit extensions update plugin-revit --debug",
             shell=True,
             creationflags=0x08000000,
         )
+        extensions.reload()
 
     except Exception as e:
         print("ERRO ao iniciar update: {}".format(e))
+
+else:
+    print("não atualizou")
